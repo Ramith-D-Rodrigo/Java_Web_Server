fd9a214a6223cc3bb4a13db956c0ee00017f69c3
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-09-10 14:53:49 +0530

    Edit Server

--- Server.java
+++ Server.java
@@ -1,13 +1,44 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
+import java.awt.Desktop;
 
 public class Server{
     public static void main(String []args) throws IOException{
         ServerSocket serverSide = new ServerSocket(2728);
-        Socket communication = serverSide.accept(); //block until the client is connected to the server (wait until the communication is established)
-
+        Socket communication = serverSide.accept(); //block until the client is connected to the server (wait until the communication is established
+        
         System.out.println("Client connected");
-        while(true){
+
+        try{
+            File index = new File("htdocs/index.html");
+            Scanner fileScan = new Scanner(index);
+            String content = "";
+            while(fileScan.hasNext()){
+                content = content + fileScan.nextLine();
+            }
+            while(true){
+                PrintWriter out = new PrintWriter(communication.getOutputStream());
+                out.println("HTTP/1.1 200 OK");
+                out.println("Content-Type: text/html");
+                out.println("\r\n");
+                out.println(content);
+                out.flush();
+                try{
+                    out.wait();
+                }
+                catch(Exception e){
+                    System.err.println(e);
+                }
+                //out.close();
+                //communication.close();
+                //serverSide.close();
+            }
+            //fileScan.close();
+        }
+        catch(FileNotFoundException e){
+            System.err.println(e);
+            System.exit(1);
         }
     }
 }
