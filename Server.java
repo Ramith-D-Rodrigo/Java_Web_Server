39a921a94eeb6d4170711399ba0a8543b624b9ae
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-09-18 10:18:49 +0530

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,7 +6,7 @@ import java.awt.Desktop;
 public class Server{
     public static void main(String []args) throws IOException{
         ServerSocket serverSide = new ServerSocket(2728);
-        Socket communication = serverSide.accept(); //block until the client is connected to the server (wait until the communication is established
+        Socket communication = serverSide.accept(); //block until the client is connected to the server (wait until the communication is established)
         
         System.out.println("Client connected");
 
